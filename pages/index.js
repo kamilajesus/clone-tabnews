109f726b4b0155b2667bb70d56ab17454fe31060
Kamila Jesus <kamilaalves727@gmail.com>
2025-10-22 04:12:17 +0000

    adiciona pages/index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Quanto mais fortes forem suas provações, maiores serão suas vitórias.</h1>
+    return <h1>Quanto mais fortes forem suas provações, maiores serão suas vitórias. Enfim é isso</h1>
 }
 
 export default Home;
