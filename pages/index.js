e53bbfc8c4747f271f388659aa9bc68b9418da41
Kamila Jesus <kamilaalves727@gmail.com>
2025-10-29 03:18:08 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,14 @@
-function Home(){
-    return <h1>Quanto mais fortes forem suas provações, maiores serão suas vitórias. Enfim é isso</h1>
+function Home() {
+  return (
+    <h1>
+      Quanto mais fortes forem suas provações, maiores serão suas vitórias.
+      Enfim é isso
+    </h1>
+  );
+}
+
+function teste() {
+  console.log("teste");
 }
 
 export default Home;
-
